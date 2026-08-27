6b7e49d4c5c1c14ba6ef62242ea0bd52dfb5bb85
prakashruthra027-star <prakashruthra027@gmail.com>
2026-08-27 11:41:25 +0530

    Update solution.sql

--- Assignment07_Marksheet/solution.sql
+++ Assignment07_Marksheet/solution.sql
@@ -1,10 +1,23 @@
 DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
--- Create Marksheet table
+CREATE TABLE Marksheet (
+    RollNo INT PRIMARY KEY,
+    Name VARCHAR(20),
+    Department VARCHAR(20),
+    Marks INT
+);
 
--- Insert sample records
+INSERT INTO Marksheet
+(RollNo, Name, Department, Marks)
+VALUES
+(1, 'Arun', 'CSE', 85),
+(2, 'Divya', 'IT', 78),
+(3, 'Karthik', 'CSE', 92),
+(4, 'Nisha', 'ECE', 67),
+(5, 'Rahul', 'IT', 88);
 
--- Display students with Marks > 80
-
--- Sort by Marks DESC
+SELECT *
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
