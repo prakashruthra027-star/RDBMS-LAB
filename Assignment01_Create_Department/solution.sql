833b620521ddb5e4911ff90a7b573a977ac39731
prakashruthra027-star <prakashruthra027@gmail.com>
2026-08-27 11:21:12 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,7 +1,13 @@
 -- Write your SQL here
 
 -- Create Database
+CREATE DATABASE CollegeDB;
 
 -- Use Database
+USE CollegeDB;
 
 -- Create Department Table
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(100) NOT NULL
+);
